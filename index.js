f88d8c78a209fbad08433780fcc1531513bd24f0
linglingliuTO <linglingliu.to@gmail.com>
2022-08-05 16:08:46 +0000

    add recurssion from n to n

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 
+
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
+/* if fromN = toN
+return fromn
+
+
+sum from (1, 1) = 1
+sum from (1, 2) = sum(1,1)+2
+sum from (1, 3 )= sum(1,2) +3
+
+from toN + sum(fromN, toN-1)
+*/
+
+
+
+if (fromN === toN) {
+  return fromN
 }
+
+return toN + sum(fromN, toN-1)
+
+
+}
+
+
 
 module.exports = sum;
